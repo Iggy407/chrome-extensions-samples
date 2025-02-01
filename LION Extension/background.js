0d4c08573a9e26fe6e45fffd2b997c4dfa4c3a4f
Iggy407 <ignacy.jemiol@gmail.com>
2025-02-01 10:19:50 +0000

    attempt 3

--- LION Extension/background.js
+++ LION Extension/background.js
@@ -1,21 +1,5 @@
-chrome.webRequest.onHeadersReceived.addListener(
-    (details) => {
-      const headers = details.responseHeaders;
-      const isXLS = headers.some(
-        (header) =>
-          header.name.toLowerCase() === "content-type" &&
-          (header.value.toLowerCase() === "application/vnd.ms-excel" ||
-            header.value.toLowerCase() ===
-              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-      );
-  
-      if (isXLS) {
-        const url = new URL(details.url);
-        const filename = url.pathname.split("/").pop();
-        chrome.storage.local.set({ xlsFilename: filename });
-        chrome.action.openPopup();
-      }
-    },
-    { urls: ["<all_urls>"] },
-    ["responseHeaders"]
-  );
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "openPopup") {
+      chrome.action.openPopup();
+    }
+  });
--- LION Extension/popup.js
+++
@@ -1,8 +0,0 @@
-chrome.storage.local.get(["xlsFilename"], (result) => {
-    const messageDiv = document.getElementById("message");
-    if (result.xlsFilename) {
-      messageDiv.textContent = `Here is an XLS file named: ${result.xlsFilename}`;
-    } else {
-      messageDiv.textContent = "No XLS file detected.";
-    }
-  });
